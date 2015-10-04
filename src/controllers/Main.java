5caf966790b2f19070e9287b01886d31b45e449f
David <David@NT>
2015-10-04 20:54:53 +0100

    fin

--- src/controllers/Main.java
+++ src/controllers/Main.java
@@ -1,5 +1,5 @@
 package controllers;
-import utils.FileLogger;
+
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
@@ -9,30 +9,28 @@ import java.util.List;
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.DomDriver;
 
-import pim.log.FileLogger;
-
-import java.util.ArrayList;
-import java.util.List;
-
+import utils.FileLogger;
 import models.User;
 
-public static void main(String[] args) throws IOException
+public class Main
 {
-  FileLogger logger = FileLogger.getLogger();
-  logger.log("Creating user list");
-
-  List<User> users = new ArrayList<User>();
-  users.add(new User("Bart", "Simpson", "bart@simpson.com", "secret"));
-  users.add(new User("Homer", "Simpson", "bart@simpson.com", "secret"));
-  users.add(new User("Lisa", "Simpson", "bart@simpson.com", "secret"));
-  System.out.println(users);
-
-  logger.log("Serializing contacts to XML");
-  XStream xstream = new XStream(new DomDriver());
-  ObjectOutputStream out = xstream.createObjectOutputStream(new FileWriter("users.xml"));
-  out.writeObject(users);
-  out.close();    
-
-  logger.log("Finished - shutting down");
+  public static void main(String[] args) throws IOException
+  {
+    FileLogger logger = FileLogger.getLogger();
+    logger.log("Creating user list");
+    
+    List<User> users = new ArrayList<User>();
+    users.add(new User("Bart", "Simpson", "bart@simpson.com", "secret"));
+    users.add(new User("Homer", "Simpson", "bart@simpson.com", "secret"));
+    users.add(new User("Lisa", "Simpson", "bart@simpson.com", "secret"));
+    System.out.println(users);
+    
+    logger.log("Serializing contacts to XML");
+    XStream xstream = new XStream(new DomDriver());
+    ObjectOutputStream out = xstream.createObjectOutputStream(new FileWriter("users.xml"));
+    out.writeObject(users);
+    out.close();    
+    
+    logger.log("Finished - shutting down");
+  }
 }
-
